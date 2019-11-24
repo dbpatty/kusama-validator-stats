b9c651c55432bdae159d7907936b1907e204aed3
joepetrowski <joe@parity.io>
2019-11-24 10:19:03 +0100

    joes first changes

--- README.md
+++ README.md
@@ -1,9 +1,56 @@
-# kusama-validator-stats
+# Getting Started
 
-To run the script, make sure you have `yarn` and `nodejs` installed.
+```
+$ yarn add typescript ts-node
+```
 
-node index.js
+To run:
+```
+$ yarn start
+```
 
 # Result
 
-![Kusama Staking Stat](https://i.imgur.com/ucKNVw4.png)
+```
+#########################################################################
+
+Stash Address: DrQHiQu5VkaRuv1H3iELXVqsvD3SV3E8xNjJqXUgECSg23R
+Self stake: 100.168928410229
+Commission: 0 KSM
+Nominators: 7
+
+Nominators
+Address: EZnvefir4bQfzEVFFdTzqjWohxM8Wpx7KxbRJJ5CHo3nCBH, Stake: 19.679900628645
+Address: HsGrsqL4nCBCW2ovc4kKG98c4mFp99BHRFkBSRZW1ETDe3U, Stake: 0.674758557459
+Address: DmfaQMFiEfPNqsR57wqoGrxgLZZ7HhE3TEqib8Mbdz2eZt1, Stake: 81.909119221704
+Address: CrqCsaJ8YQqPEUBDaL1y9hLDS9suHBP1qoamNmF7NqttbiC, Stake: 231.439173438009
+Address: ELharp1RuLgHUE6436sWSpW6XB2BqhaSq9L1Wexs83aodZ6, Stake: 0.538701561611
+Address: GZYgwXwUXtaojk4nzidReDWMcKbmeus8zGTcpH9tpDDCj2S, Stake: 1.298587679278
+Address: GMHFpxFvhGPRWVHDL2U2C7LKpXSqaweUrV6fUhckn2bTY4P, Stake: 301.140777684972
+
+Validator total stake: 736.849947181907 KSM
+
+#########################################################################
+
+Stash Address: EFeCdfpLENVJJNajoDCvSJ9f9CwutiaDeUFxWjA54kWEG17
+Self stake: 9.845488055991
+Commission: 0 KSM
+Nominators: 5
+
+Nominators
+Address: DrBXD1GYVfJSsmxS2c9GEipMA2KCh6TgK359nsn7297HRo8, Stake: 818.120802749624
+Address: EZnvefir4bQfzEVFFdTzqjWohxM8Wpx7KxbRJJ5CHo3nCBH, Stake: 19.223176990002
+Address: GX7C1muSs6928UZzUgrVWJ2JMkDWQEhTRGXjnwaULPDSh2y, Stake: 32.283966453844
+Address: GZYgwXwUXtaojk4nzidReDWMcKbmeus8zGTcpH9tpDDCj2S, Stake: 0.304457385783
+Address: F7CoSrkthA74hV3xmxrChK1b5eBYkScuFsUpUVqd6BLeyGU, Stake: 0.09001980783
+
+Validator total stake: 879.867911443074 KSM
+
+#########################################################################
+
+Total KSM: 7878834.5944481045
+Bonding Stake: 2909886.1344865407 KSM
+Staking Rate: 36.932951182107814 %
+Validators: 130
+Minimum stake: 736.849947181907
+```
---
+++ index.ts
@@ -0,0 +1,51 @@
+const{ ApiPromise, WsProvider } = require('@polkadot/api');
+const { isHex } = require('@polkadot/util');
+const DOT_DECIMAL_PLACES = 1000000000000;
+let display_nominators = true;
+
+(async () => {
+	const provider = new WsProvider('wss://kusama-rpc.polkadot.io/');
+	const api = await ApiPromise.create({ provider });
+
+	const [ currentValidators, totalIssuance ] = await Promise.all([
+		api.query.session.validators(),
+		api.query.balances.totalIssuance()
+	]);
+
+	const totalKSM = parseInt(totalIssuance.toString());
+	let totalBondingStake = 0;
+	let stakes = [];
+
+	for (let i=0; i<currentValidators.length; i++) {
+		const validatorTotalStake = await api.query.staking.stakers(currentValidators[i]);
+		const validatorCommissioneRate = await api.query.staking.validators(currentValidators[i]);
+
+		console.log(`Stash Address: ${currentValidators[i].toString()}`);
+		console.log(`Self Stake: ${validatorTotalStake['own'].toString() / DOT_DECIMAL_PLACES}`);
+		console.log(`Commission: ${validatorCommissioneRate[0]['validatorPayment'] / DOT_DECIMAL_PLACES} KSM`);
+		console.log(`Nominators: ${validatorTotalStake['others'].length}\n`);
+
+		// @ts-ignore
+		if (display_nominators === true) {
+			console.log(`Nominators`);
+			for (let j=0; j<validatorTotalStake['others'].length; j++) {
+				console.log(`Address: ${validatorTotalStake['others'][j]['who']},` +
+					` Stake: ${validatorTotalStake['others'][j]['value'] / DOT_DECIMAL_PLACES}`
+				);
+			}
+			console.log(` `);
+		}
+
+		console.log(`Validator total stake: ${validatorTotalStake['total'] / DOT_DECIMAL_PLACES} KSM\n`);
+		stakes.push(validatorTotalStake['total'] / DOT_DECIMAL_PLACES);
+		totalBondingStake += parseInt(validatorTotalStake['total']);
+
+		console.log("#########################################################################\n");
+	}
+
+	console.log(`Total KSM: ${totalKSM / DOT_DECIMAL_PLACES}`);
+	console.log(`Bonding Stake: ${totalBondingStake / DOT_DECIMAL_PLACES} KSM`);
+	console.log(`Staking Rate: ${totalBondingStake / totalKSM * 100} %`);
+	console.log(`Validators: ${currentValidators.length}`);
+	console.log(`Minimum stake: ${Math.min.apply(null, stakes)}`);
+})()
