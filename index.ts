f50d791fa1747b3e1a045eeb61e53f9734a12327
joepetrowski <joe@parity.io>
2019-11-24 16:33:33 +0100

    prettier formatting

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 const{ ApiPromise, WsProvider } = require('@polkadot/api');
 const { isHex } = require('@polkadot/util');
-const DOT_DECIMAL_PLACES = 1000000000000;
+const DOT_DECIMAL_PLACES = 1_000_000_000_000;
 
 let display_nominators = true;
 const provider = new WsProvider('wss://kusama-rpc.polkadot.io/');
+
+function formatWithCommas(num) {
+	return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
 
 async function main(){
 	
@@ -20,36 +24,47 @@ async function main(){
 
 	for (let i=0; i<currentValidators.length; i++) {
 		const validatorTotalStake = await api.query.staking.stakers(currentValidators[i]);
-		const validatorCommissioneRate = await api.query.staking.validators(currentValidators[i]);
+		const validatorCommissionRate = await api.query.staking.validators(currentValidators[i]);
+
+		let self_stake = (validatorTotalStake['own'].toString() / DOT_DECIMAL_PLACES).toFixed(2);
+		let commission = (validatorCommissionRate[0]['validatorPayment'] / DOT_DECIMAL_PLACES).toFixed(2);
 
 		console.log(`Stash Address: ${currentValidators[i].toString()}`);
-		console.log(`Self Stake: ${validatorTotalStake['own'].toString() / DOT_DECIMAL_PLACES}`);
-		console.log(`Commission: ${validatorCommissioneRate[0]['validatorPayment'] / DOT_DECIMAL_PLACES} KSM`);
+		console.log(`Self Stake: ${formatWithCommas(self_stake)} KSM`);
+		console.log(`Commission: ${formatWithCommas(commission)} KSM`);
 		console.log(`Nominators: ${validatorTotalStake['others'].length}\n`);
 
 		// @ts-ignore
 		if (display_nominators === true) {
 			console.log(`Nominators`);
 			for (let j=0; j<validatorTotalStake['others'].length; j++) {
-				console.log(`Address: ${validatorTotalStake['others'][j]['who']},` +
-					` Stake: ${validatorTotalStake['others'][j]['value'] / DOT_DECIMAL_PLACES}`
+				let nominator_stake = (validatorTotalStake['others'][j]['value'] / DOT_DECIMAL_PLACES).toFixed(2);
+				console.log(`Address: ${validatorTotalStake['others'][j]['who']}, ` +
+					`Stake: ${formatWithCommas(nominator_stake)}`
 				);
 			}
 			console.log(` `);
 		}
 
-		console.log(`Validator total stake: ${validatorTotalStake['total'] / DOT_DECIMAL_PLACES} KSM\n`);
+		let val_total_stake = (validatorTotalStake['total'] / DOT_DECIMAL_PLACES).toFixed(2);
+
+		console.log(`Validator total stake: ${formatWithCommas(val_total_stake)} KSM\n`);
 		stakes.push(validatorTotalStake['total'] / DOT_DECIMAL_PLACES);
 		totalBondingStake += parseInt(validatorTotalStake['total']);
 
 		console.log("#########################################################################\n");
 	}
 
-	console.log(`Total KSM: ${totalKSM / DOT_DECIMAL_PLACES}`);
-	console.log(`Bonding Stake: ${totalBondingStake / DOT_DECIMAL_PLACES} KSM`);
-	console.log(`Staking Rate: ${totalBondingStake / totalKSM * 100} %`);
-	console.log(`Validators: ${currentValidators.length}`);
-	console.log(`Minimum stake: ${Math.min.apply(null, stakes)}`);
+	let total_ksm = (totalKSM / DOT_DECIMAL_PLACES).toFixed(0);
+	let bonding_stake = (totalBondingStake / DOT_DECIMAL_PLACES).toFixed(0);
+	let staking_rate = (totalBondingStake / totalKSM * 100).toFixed(1);
+	let minimum_stake = (Math.min.apply(null, stakes)).toFixed(2);
+
+	console.log(`Total Issuance: ${formatWithCommas(total_ksm)} KSM`);
+	console.log(`Bonding Stake:  ${formatWithCommas(bonding_stake)} KSM`);
+	console.log(`Staking Rate:   ${staking_rate} %`);
+	console.log(`Validators:     ${currentValidators.length}`);
+	console.log(`Minimum Stake:  ${formatWithCommas(minimum_stake)}\n`);
 
 	process.exit(0);
 }
